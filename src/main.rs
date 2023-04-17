e4c03c0e8721aecea30e119c423329ec9eca7184
torijacarlos <alejandro.torija.b@gmail.com>
2023-04-16 19:06:15 -0600

    noop:todo

--- src/main.rs
+++ src/main.rs
@@ -15,3 +15,10 @@ async fn main() -> DkkResult<()> {
 
 // @todo: figure out what to do with ui, I don't want to invest a lot on it for now, but I think
 // the next steps require some sort of polish. take notes on each scene and act on that maybe
+//
+//
+// @todo: the past rabbit hole made me think, do I even need a database? 
+// I'll try to make it an option. or should i delete the entire db implementation?
+// where and why would I need it? The file could get a bit big, but even if it gets to 1GB, I could
+// start splitting it into separate files
+// I struggled so much with the threading that it pains me to do so 
