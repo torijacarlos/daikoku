16a95657deefc3acb852d20c4b1175aaa9c290ff
torijacarlos <alejandro.torija.b@gmail.com>
2023-04-16 19:06:15 -0600

    noop:todo

--- src/main.rs
+++ src/main.rs
@@ -12,3 +12,9 @@ async fn main() -> DkkResult<()> {
     )
     .map_err(DkkError::Render)
 }
+
+// @todo: figure out what to do with ui, I don't want to invest a lot on it for now, but I think
+// the next steps require some sort of polish. take notes on each scene and act on that maybe
+//
+// @todo: export to encrypted file
+// @todo: import it to database
